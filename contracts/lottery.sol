2ba0cd2ee06131a4d36075d86d8c0b2cc8bc639f
Amitesh Thind <thindamitesh@gmail.com>
2023-06-19 13:19:54 -0400

    created new project and wrote down some pseudo code

---
+++ contracts/lottery.sol
@@ -0,0 +1,8 @@
+//Raffle Contract
+
+//fund x amount of money per user 
+//winner selected after X minutes(time) --> must be automated
+//randomly pick a winner
+
+//chainlink oracle ->randomness,Automated exeuction (chainlink keepers) (need someone to execute the smart contract)
+ 
